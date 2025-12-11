0c16a76735196dec1303f5690c2b37619b30d883
Debjyoti Banerjee <mr.db.cse@gmail.com>
2025-12-11 21:09:33 +0530

    ejs completed

--- 3-ejs-concepts/index.js
+++ 3-ejs-concepts/index.js
@@ -1,6 +1,25 @@
 import express from "express";
-import path from "path";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// console.log(__dirname);
 
 app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
+
+const users = [
+  { id: 1, name: "Carthick" },
+  { id: 2, name: "Acubee" },
+];
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home", users: users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
